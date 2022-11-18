b52010a3e288e0d0d0906eafbf80d4b4fb144547
Lux <58230822+Lux-0@users.noreply.github.com>
2022-11-18 17:07:34 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Linq;
+using System.Threading;
+
+namespace RAMCollector
+{
+    class Program
+    {
+        static string ConfPath;
+        static List<string> processes;
+
+        [DllImport("kernel32.dll")]
+        static extern bool SetProcessWorkingSetSize(IntPtr proc, int min, int max);
+
+        [DllImport("kernel32.dll")]
+        static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        public static int GetProcess(string processName)
+        {
+            int ProcessID = -1;
+            foreach (Process Process in Process.GetProcessesByName(processName))
+            {
+                if (Process.WorkingSet64 > 0)
+                {
+                    ProcessID = Process.Id;
+                }
+            }
+            return ProcessID;
+        }
+
+        public static void HandleProcesses()
+        {
+            processes = System.IO.File.ReadAllLines(ConfPath).ToList();
+            processes.ForEach(ProcessName =>
+            {
+                new Thread(() =>
+                {
+                    Thread.CurrentThread.IsBackground = true;
+                    while (GetProcess(ProcessName) != -1)
+                    {
+                        if (GetProcess(ProcessName) != -1)
+                        {
+                            GC.Collect();
+
+                            GC.WaitForPendingFinalizers();
+
+                            if (Environment.OSVersion.Platform == PlatformID.Win32NT)
+                            {
+                                SetProcessWorkingSetSize(Process.GetProcessById(GetProcess(ProcessName)).Handle, -1, -1);
+                            }
+
+                            Thread.Sleep(1);
+                        }
+                    }
+                }).Start();
+                Console.WriteLine("Managing RAM Usage for " + ProcessName);
+            });
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Title = "RAM Collector By Lux";
+            string LocalConfPath = System.IO.Directory.GetParent(System.Reflection.Assembly.GetEntryAssembly().Location).ToString() + "\\RAMCollector.conf";
+            if (!System.IO.File.Exists(LocalConfPath))
+            {
+                System.IO.File.WriteAllLines(ConfPath, new String[] { "discord", "chrome", "obs64" });
+                Console.WriteLine("Created a config file with default executables.");
+                Console.ReadLine();
+                Environment.Exit(0);
+            } else ConfPath = LocalConfPath;
+
+            processes = System.IO.File.ReadAllLines(ConfPath).ToList();
+            HandleProcesses();
+            Console.WriteLine("Press enter to hide window");
+            Console.ReadLine();
+            ShowWindow(GetConsoleWindow(), 0);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Ram Limiter
+
+## Description
+Limits any specified applications ram usages using Garbage Collectors (GC)
+
+## Use
+Frees RAM being used by all specificed applications automatically.
+This can help if your device does not have enough RAM to run everything smoothly.
+This could also be used if you just want to free RAM for per-say a RAM intensive game. 
